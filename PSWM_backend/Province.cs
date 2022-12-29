e51aacb689761b7fd707a531eadbf77d0af58bf5
Eliebm <eliebm17@live.com>
2022-12-29 13:55:56 +0200

    Add project files.

---
+++ PSWM_backend/Province.cs
@@ -0,0 +1,10 @@
+﻿namespace PSWM_backend_project
+{
+    public class Province
+    { 
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+       
+    }
+}
---
+++ PSWM_backend/Startup.cs
@@ -0,0 +1,12 @@
+﻿namespace PSWM_backend_project
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+    }
+}
